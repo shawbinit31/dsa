86345774bdcae062da8df5af0e557a3c0e0ce299
shawbinit31 <shawbinit31@gmail.com>
2026-09-17 21:30:46 +0530

    Time: 4 ms (30.72%) | Memory: 46.3 MB (60.71%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String intToRoman(int num) {
+        // Values and corresponding Roman symbols in descending order
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder sb = new StringBuilder();
+
+        // Greedily subtract the largest possible symbol value
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                sb.append(symbols[i]);
+            }
+        }
+
+        return sb.toString();
+    }
+}
