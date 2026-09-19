27dee42c1044d8ca283ecd60cd9f75b4d3a7378e
shawbinit31 <shawbinit31@gmail.com>
2026-09-19 22:33:03 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (79.81%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, new StringBuilder(), 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, StringBuilder current, int open, int close, int max) {
+        // Base case: length is 2 * n
+        if (current.length() == max * 2) {
+            result.add(current.toString());
+            return;
+        }
+
+        // Option 1: Add opening bracket if we haven't used all n
+        if (open < max) {
+            current.append('(');
+            backtrack(result, current, open + 1, close, max);
+            current.deleteCharAt(current.length() - 1); // backtrack
+        }
+
+        // Option 2: Add closing bracket if it won't exceed opening brackets
+        if (close < open) {
+            current.append(')');
+            backtrack(result, current, open, close + 1, max);
+            current.deleteCharAt(current.length() - 1); // backtrack
+        }
+    }
+}
